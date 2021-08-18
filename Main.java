012bbc59fb95df4639b58c9f5edf1ec6a1d74f58
lucadonn <63601735+lucadonn@users.noreply.github.com>
2021-08-18 15:29:57 +1000

    completed task 3

---
+++ Main.java
@@ -0,0 +1,39 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) throws Exception {
+        Main window = new Main();
+        //window.run();
+    }
+
+    public class Canvas extends JPanel {
+        public Canvas(){
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        @Override
+        public void paint(Graphics g){
+            
+            for(int i = 10; i<710; i+=35){
+                for(int j = 10; j < 710; j+=35){
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i, j, 35, 35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35, 35);
+            }
+        }
+        }
+    }
+
+    public Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+
+}
+
